6f25e318b401e0b5e2f21a3a03906cd25a9fea18
Travis Puryear <travispuryear678@gmail.com>
2021-10-29 16:43:42 -0400

    folder creation and begin of User model.

---
+++ controllers/homeRoute.js
@@ -0,0 +1,3 @@
+const sequelize = require('../config/connection')
+const router = require('express').Router
+const {User} = require('../models')
---
+++ models/User.js
@@ -0,0 +1,8 @@
+const { Model, DataTypes } = require("sequelize/types");
+const sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+
+
+
+
+module.exports = User
