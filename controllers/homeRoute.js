e49a1a83e415328b5093d62c3856d5f17c0b6cb8
Travis Puryear <travispuryear678@gmail.com>
2021-10-30 14:31:46 -0400

    Public folder and linking of style sheets to layout main.handlebars

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,6 +1,17 @@
 const sequelize = require('../config/connection')
 const router = require('express').Router()
-const { User } = require('../models')
+const { User,Post,Comment } = require('../models')
+
+
+router.get('/', async (req,res) => {
+    const userData = await User.findAll().catch((err) => {
+        res.status(500).json(err)
+    })
+    const user = userData.map((userinfo) => userinfo.get({plain:true}));
+    
+    res.status(200).render('homepage',{user});
+})
+
 
 
 module.exports = router;
