7f2762241c2bdd819434ae70b0614c6ddb2251a1
Travis Puryear <travispuryear678@gmail.com>
2021-10-30 14:09:58 -0400

    API routes intial setup.

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
 
-const apiRoute = require('./api')
-const homeRoute = require('./homeRoute.js')
-const postsRoute = require('./allPosts.js')
+const apiRoute = require('./api');
+const homeRoute = require('./homeRoute.js');
+const postsRoute = require('./allPosts.js');
+const notFound = require('./404page.js');
 
 
-
-router.use("/",homeRoute)
-router.use("/api",apiRoute)
-router.use("/posts",postsRoute)
-
-
+router.use("/",homeRoute);
+router.use("/api",apiRoute);
+router.use("/posts",postsRoute);
+router.use('/404', notFound);
 
 
 
