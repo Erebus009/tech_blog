37fd3c9358d9c934edafffb8835199d0d95e038f
Travis Puryear <travispuryear678@gmail.com>
2021-10-31 01:41:48 -0400

    signup creates user and session.login works correctly.

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -53,7 +53,14 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
 
-
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
 
 
 
